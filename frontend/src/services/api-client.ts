3c86f105b283b40b05acea643d3bfadac1fa15ee
Nick <nickphilomath@gmail.com>
2024-01-24 12:31:37 +0500

    cleaned public folder in backend; updated doker-compose file

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.REACT_APP_API_URL || "http://127.0.0.1:8000"
+const baseUrl = import.meta.env.MODE === 'developer' ? "http://127.0.0.1:8000" : "";
 // export const baseUrl = "";
 
 export default axios.create({
